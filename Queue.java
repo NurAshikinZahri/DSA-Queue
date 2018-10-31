480cb136a7eff1959c3c107347987274e1b7bbf6
NurAshikinZahri <44635282+NurAshikinZahri@users.noreply.github.com>
2018-10-31 16:31:12 +0800

    Add files via upload

---
+++ Queue.java
@@ -0,0 +1,11 @@
+
+package queue;
+
+public interface Queue {
+    
+    public void add(Object object);
+    public Object first();
+    public Object remove();
+    public int size();
+    public boolean isEmpty();
+}
